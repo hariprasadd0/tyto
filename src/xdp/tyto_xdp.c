59c543a1bb8b0c2bbc8e19f6d8586e5452eb45e8
hariprasad <hariprasadkv02@gmail.com>
2026-04-03 17:30:29 +0000

    add udp track bpf map

--- src/xdp/tyto_xdp.c
+++ src/xdp/tyto_xdp.c
@@ -14,6 +14,7 @@
 #define UDP_RATE_LIMIT  800
 #define SYN_RATE_LIMIT  100
 #define ICMP_RATE_LIMIT  500
+#define MAX_ENTRIES  65536
 
 
 
@@ -25,3 +26,18 @@ struct tyto_event {
     __u8 protocol; // TCP, UDP, ICMP
     __u8 pad[3];
 };
+
+//Per-IP state struct
+struct ip_entry {
+    __u64 window_start;
+    __u32 pkt_count;
+    __u32 blocked; // 0 = not blocked, 1 = blocked
+};
+
+//BPF maps
+struct {
+__uint(type, BPF_MAP_TYPE_LRU_HASH);
+__uint(max_entries, MAX_ENTRIES);
+__type(key, __u32);
+__type(value, struct ip_entry);
+}udp_track_map SEC(".maps");
